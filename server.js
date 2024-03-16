3395fa194a27d891609b639a0220f44d960c02d2
dtscott9 <dtscott9@byui.edu>
2024-03-15 19:08:31 -0600

    connected to database

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongodb = require("./db/connect");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port, () => {
+        console.log(`Web Server is listening at http://localhost:${port}/`);
+      });
+    }
+  });
