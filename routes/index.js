cf43e3d5c314dd91b8bce4e6bba5ce8c451f4dd3
dtscott9 <dtscott9@byui.edu>
2024-03-21 19:20:09 -0600

    implemented auth0

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const { auth } = require('express-oauth2-jwt-bearer');
 
-router.use('/', require('./swagger'));
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+    audience: process.env.AUDIENCE,
+    issuerBaseURL: `https://${process.env.ISSUER_BASE_URL}`
+});
+
+
+router.use('/api', checkJwt, require('./swagger'));
 // router.use('/posts', require('./posts'));
 // router.use('/users', require('./users'));
 // router.use('/followers', require('./followers'));
