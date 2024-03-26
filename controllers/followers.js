96d3c2d9720756d7034c7f5fa245a21e890d777b
Austin Nelson <nel.austintyler.son@gmail.com>
2024-03-26 15:04:27 -0600

    controller-functionality

--- controllers/followers.js
+++ controllers/followers.js
@@ -1,9 +1,9 @@
 
-const mongodb = require('mongodb');
+const mongodb = require('../db/connect');
 const ObjectId = mongodb.ObjectId;
 
 const getAllFollowers = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').find();
+    const result = await mongodb.getDb().db('frogforum').collection('followers').find();
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
@@ -11,25 +11,38 @@ const getAllFollowers = async (req, res) => {
   };
 
 const getOneFollowers = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').findOne({ _id: ObjectId(req.params.id) });
+    const result = await mongodb.getDb().db('frogforum').collection('followers').findOne({ _id: ObjectId(req.params.id) });
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
     }
 
 const createFollowers = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').insertOne(req.body);
+    const followers = {
+      userId: req.body.userId,
+      followers: req.body.followers
+    }
+    const result = await mongodb.getDb().db('frogforum').collection('followers').insertOne(followers);
     res.setHeader('Content-Type', 'application/json');
     res.status(201).json(result);
   }
 
 const updateFollowers = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body });
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Invalid user id. Please try again.');
   }
+  const userId = new ObjectId(req.params.id);
+  const followers = {
+    userId: req.body.userId,
+    followers: req.body.followers
+  }
+  const result = await mongodb.getDb().db('frogforum').collection('followers').replaceOne({ _id: userId }, followers);
+  console.log(result);
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(result);
+}
 
 const removeFollowers = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').deleteOne({ _id: ObjectId(req.params.id) });
+    const result = await mongodb.getDb().db('frogforum').collection('followers').deleteOne({ _id: ObjectId(req.params.id) });
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
   }
--- controllers/following.js
+++ controllers/following.js
@@ -1,8 +1,8 @@
-const mongodb = require('mongodb');
+const mongodb = require('../db/connect');
 const ObjectId = mongodb.ObjectId;
 
 const getAllFollowing = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').find();
+    const result = await mongodb.getDb().db('frogforum').collection('following').find();
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
@@ -10,25 +10,38 @@ const getAllFollowing = async (req, res) => {
   };
 
 const getOneFollowing = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').findOne({ _id: ObjectId(req.params.id) });
+    const result = await mongodb.getDb().db('frogforum').collection('following').findOne({ _id: ObjectId(req.params.id) });
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
     }
 
 const createFollowing = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').insertOne(req.body);
+    const following = {
+      userId: req.body.userId,
+      following: req.body.following
+    }
+    const result = await mongodb.getDb().db('frogforum').collection('following').insertOne(following);
     res.setHeader('Content-Type', 'application/json');
     res.status(201).json(result);
   }
 
 const updateFollowing = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body });
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Invalid user id. Please try again.');
+    }
+    const userId = new ObjectId(req.params.id);
+    const following = {
+      userId: req.body.userId,
+      following: req.body.following
+    }
+    const result = await mongodb.getDb().db('frogforum').collection('following').replaceOne({ _id: userId }, following);
+    console.log(result);
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
   }
 
 const removeFollowing = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('followers').deleteOne({ _id: ObjectId(req.params.id) });
+    const result = await mongodb.getDb().db('frogforum').collection('following').deleteOne({ _id: ObjectId(req.params.id) });
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
   }
