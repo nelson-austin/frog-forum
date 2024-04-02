0c3bcbf5e8cfb5e3af79286011bbd4bee43ba8c0
dtscott9 <dtscott9@byui.edu>
2024-04-01 20:03:42 -0600

    added error handling to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -3,27 +3,45 @@ const ObjectId = require("mongodb").ObjectId;
 const dbFunctions = require("./index");
 
 const getAllUsers = async (req, res) => {
-  const result = await dbFunctions.getMongoDb("users").find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await dbFunctions.getMongoDb("users").find();
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch (e) {
+    console.log("error", e);
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists);
-  });
+    res.status(404).json(JSON.stringify(e));
+  }
 };
 
 const getOneUserByAuth0 = async (req, res) => {
-  const result = await dbFunctions
-    .getMongoDb("users")
-    .findOne({ auth0Id: req.params.auth0id });
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json(result);
+  try {
+    const result = await dbFunctions
+      .getMongoDb("users")
+      .findOne({ auth0Id: req.params.auth0id });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(result);
+  } catch (e) {
+    console.log("error", e);
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).json(JSON.stringify(e));
+  }
 };
 
 const getOneUser = async (req, res) => {
-  const result = await dbFunctions
-    .getMongoDb("users")
-    .findOne({ _id: new ObjectId(req.params.id) });
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json(result);
+  try {
+    const result = await dbFunctions
+      .getMongoDb("users")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(result);
+  } catch (e) {
+    console.log("error", e);
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).json(JSON.stringify(e));
+  }
 };
 
 const createUser = async (req, res) => {
@@ -37,6 +55,15 @@ const createUser = async (req, res) => {
   };
   const result = await dbFunctions.getMongoDb("users").insertOne(user);
   res.setHeader("Content-Type", "application/json");
+  if (result.acknowledged) {
+    res.status(201).json(result);
+  } else {
+    res
+      .status(500)
+      .json(
+        result.error || "An error occurred while trying to create the user"
+      );
+  }
   res.status(201).json(result);
 };
 
@@ -48,7 +75,15 @@ const updateUser = async (req, res) => {
     { $set: req.body }
   );
   res.setHeader("Content-Type", "application/json");
-  res.status(200).json(result);
+  if (result.modifiedCount > 0) {
+    res.status(204).json(result);
+  } else {
+    res
+      .status(400)
+      .json(
+        result.error || "An error occurred while trying to update user info"
+      );
+  }
 };
 
 const removeUser = async (req, res) => {
@@ -56,7 +91,15 @@ const removeUser = async (req, res) => {
     _id: new ObjectId(req.params.id),
   });
   res.setHeader("Content-Type", "application/json");
-  res.status(200).json(result);
+  if (result.deletedCount > 0) {
+    res.status(202).json(result);
+  } else {
+    res
+      .status(404)
+      .json(
+        result.error || "An error occurred while trying to delete the user"
+      );
+  }
 };
 
 module.exports = {
